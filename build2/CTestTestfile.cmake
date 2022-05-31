b0714736bb987c39c649338fb3ff1636dddc2148
MartijnEisses <33952780+MartijnEisses@users.noreply.github.com>
2022-05-31 15:26:59 +0200

    renamed build2 back to build

--- build2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Programming/Projects/openpilot
-# Build directory: D:/Programming/Projects/openpilot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
